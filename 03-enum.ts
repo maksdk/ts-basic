2a6f6c53e82168663867db1dd4dc557690991a5f
maksdk <havryshchenko@gmail.com>
2019-12-18 13:12:54 +0200

    enum

---
+++ 03-enum.ts
@@ -0,0 +1,16 @@
+enum Membership {
+    Simple,
+    Standart,
+    Premium
+}
+
+const membership = Membership.Standart;
+const membershipReverse = Membership[2];
+
+enum SocialMedia {
+    VK = "Vkontakte",
+    FACEBOOK = "FACEBOOK",
+    INSTA = "INSTA"
+}
+
+const socMedia = SocialMedia.INSTA
